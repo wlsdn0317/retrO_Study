9cbcfc8416196fd7105c4ce42f968f2e5a20ab6d
wlsdn0317 <78064428+wlsdn0317@users.noreply.github.com>
2023-10-24 16:44:53 +0900

    Awake,Start
    Awake,Start 주기

---
+++ Hello Unity/Assets/02.Script/AwakeTest.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AwakeTest : MonoBehaviour
+{
+    void Awake() 
+    {
+        Debug.Log("Awake_1");;    
+    }
+
+
+    void Start()
+    {
+        Debug.Log("Start_1");
+    }
+}
---
+++ Hello Unity/Assets/02.Script/StartTest.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartTest : MonoBehaviour
+{
+    void Awake()
+    {
+        Debug.Log("Awakw_2");        
+    }
+
+    void Start()
+    {
+        Debug.Log("Start_2");
+    }
+
+}
